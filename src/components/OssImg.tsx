c5b56f189cbb57e9c2f70e191e1c1f77497ec75c
liubo <1060247260@qq.com>
2022-02-23 18:04:46 +0800

    dist deploy

--- src/components/OssImg.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const host = 'https://wehome-image-prd.oss-cn-hangzhou.aliyuncs.com/';
-
-export default ({ src, ...rest }: { src: string }) => {
-  return <img {...rest} src={host + src} />;
-};
--- src/components/PageLoading/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// loading components from code split
-// https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
-export default () => (
-  <div className="flex justify-center items-center h-screen">loading...</div>
-);
